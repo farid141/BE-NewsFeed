a91c1e930b5a20199c719ca5b4b0555b926dfe1f
farid nubaili <farid.nubaili@tritronik.com>
2025-11-12 13:43:09 +0700

    add note to auth controller

--- controller/auth_controller.go
+++ controller/auth_controller.go
@@ -8,6 +8,7 @@ import (
 	"github.com/farid141/go-rest-api/dto"
 	"github.com/farid141/go-rest-api/helper"
 	"github.com/farid141/go-rest-api/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 
@@ -18,16 +19,24 @@ func Login(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		var err error
 
-		type Req struct {
-			Username string `json:"username"`
-			Password string `json:"password"`
-		}
-
-		var req Req
+		// Parsing
+		var req dto.CreateUserRequest
 		if err := c.BodyParser(&req); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 		}
 
+		// Validation
+		// 1. Structure validation
+		func() error {
+			var validate *validator.Validate = validator.New()
+			err = validate.Struct(req)
+			if err != nil {
+				return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error": "invalid input validation"})
+			}
+			return nil
+		}()
+
+		// 2. DB Validation
 		var userID int
 		var hashedPassword string
 		err = db.QueryRow(
@@ -35,26 +44,30 @@ func Login(db *sql.DB) fiber.Handler {
 			req.Username,
 		).Scan(&userID, &hashedPassword)
 		if err != nil {
-			return c.Status(401).JSON(fiber.Map{"error": "invalid credentials"})
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal Server Error"})
 		}
 
+		// Processing
+		// 1. password matching
 		err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(req.Password))
 		if err != nil {
 			return c.Status(401).JSON(fiber.Map{"error": "Invalid credentials"})
 		}
 
-		// token
+		// 2. generate token
 		token, err := utils.GenerateJWT(req.Username, strconv.Itoa(userID), 15*time.Minute)
 		if err != nil {
 			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
-		// refresh_token
-		c.Cookie(&fiber.Cookie{Name: "token", Value: token})
+		// 3. generate refresh_token
 		refresh_token, err := utils.GenerateJWT(req.Username, strconv.Itoa(userID), 7*24*time.Hour)
 		if err != nil {
 			return c.SendStatus(fiber.StatusInternalServerError)
 		}
+
+		// structuring response
+		c.Cookie(&fiber.Cookie{Name: "token", Value: token})
 		c.Cookie(&fiber.Cookie{Name: "refresh_token", Value: refresh_token})
 
 		return c.JSON(fiber.Map{"token": token})
@@ -65,12 +78,13 @@ func Register(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		var err error
 
+		// Parsing
 		var req dto.CreateUserRequest
 		if err := c.BodyParser(&req); err != nil {
-			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 		}
 
-		// username validation
+		// DB Validation
 		exists, err := helper.CoulmnValueExists(db, "users", "username", req.Username)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -83,12 +97,14 @@ func Register(db *sql.DB) fiber.Handler {
 			})
 		}
 
+		// Processing
+		// 1. generate hash password
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
 		if err != nil {
 			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
 		}
 
-		// insert new user
+		// 2. insert new user
 		res, err := db.Exec(
 			`INSERT INTO users (username, password_hash, created_at) VALUES (?,?,NOW())`,
 			req.Username,
@@ -100,6 +116,7 @@ func Register(db *sql.DB) fiber.Handler {
 			})
 		}
 
+		// 3. getting last inserted data id
 		id, err := res.LastInsertId()
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -107,6 +124,7 @@ func Register(db *sql.DB) fiber.Handler {
 			})
 		}
 
+		// structuring response
 		return c.Status(fiber.StatusCreated).JSON(dto.UserResponse{
 			ID:       id,
 			Username: req.Username,
@@ -115,11 +133,13 @@ func Register(db *sql.DB) fiber.Handler {
 }
 
 func RefreshToken(c *fiber.Ctx) error {
+	// Parsing
 	refreshCookie := c.Cookies("refresh_token")
 	if refreshCookie == "" {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "no refresh token"})
 	}
 
+	// Input validation
 	token, err := jwt.Parse(refreshCookie, func(t *jwt.Token) (interface{}, error) {
 		return []byte("secret"), nil
 	})
@@ -127,15 +147,16 @@ func RefreshToken(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid refresh token"})
 	}
 
+	// Process
 	claims := token.Claims.(jwt.MapClaims)
 	username := claims["username"].(string)
 	userId := claims["id"].(string)
-
 	newAccessToken, err := utils.GenerateJWT(username, userId, 15*time.Minute)
 	if err != nil {
 		return err
 	}
 
+	// structuring response
 	c.Cookie(&fiber.Cookie{Name: "token", Value: newAccessToken})
 
 	return c.JSON(fiber.Map{"message": "token refreshed"})
